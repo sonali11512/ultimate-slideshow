a1c252f8ed689b53e356dd243b7eb48be2a83080
sonali11512 <sonali.1215@gmail.com>
2019-05-21 22:28:41 +0530

    changing the path in bootstrap file to sync with travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,11 @@
  * @package Ultimate_Slideshow
  */
 
-$_tests_dir = "C:\\xampp\htdocs\\rtcamp\wp-content\plugins\ultimate-slideshow\\tests\wordpress-tests-lib";
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	// $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
