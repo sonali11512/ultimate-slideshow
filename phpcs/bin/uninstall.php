64556a6e059ec40a44b169e3293b51b57dee273b
sonali11512 <sonali.1215@gmail.com>
2019-05-22 13:48:09 +0530

    testing

---
+++ phpcs/bin/uninstall.php
@@ -0,0 +1,12 @@
+
+FILE: ...mp\wp-content\plugins\ultimate-slideshow\phpcs\bin\uninstall.php
+----------------------------------------------------------------------
+FOUND 0 ERRORS AND 1 WARNING AFFECTING 1 LINE
+----------------------------------------------------------------------
+ 1 | WARNING | No PHP code was found in this file and short open tags
+   |         | are not allowed by this install of PHP. This file may
+   |         | be using short open tags but PHP does not allow them.
+----------------------------------------------------------------------
+
+Time: 543ms; Memory: 8MB
+
